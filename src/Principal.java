22ffda68cd53008af9d7e8edd01e1a8751b8c265
EdsonCasta <dedsoncasta07@gmail.com>
2025-05-09 14:01:19 -0500

    Consultar la pelicula

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Escribe el numero de la pelicula de star wars que quieres consultar: ");
+
+        try {
+            var numeroDePelicula = Integer.valueOf(scanner.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+        } catch (NumberFormatException e) {
+            System.out.println("Numero no encontrado " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación");
+        }
     }
 }
